92099e58f6dd32cae533ba329165f6f12abab687
Ramzi Samman <ramzy.sa@gmail.com>
2017-03-10 22:20:11 +0200

    Set basePath config
    When setting the editor mode to json, ace tries to load /mode-json.js
    Setting basePath config fixes the issue.
    Tested with angular cli

--- ng2-ace-editor.ts
+++ ng2-ace-editor.ts
@@ -3,3 +3,6 @@ import {AceEditorComponent} from './src/component';
 import {AceEditorModule} from './src/module';
 
 export {AceEditorModule, AceEditorDirective, AceEditorComponent};
+
+declare var ace: any;
+ace.config.set('basePath', 'node_modules/ace-builds/src/');
